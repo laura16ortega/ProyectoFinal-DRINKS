3c93f5ab4549cc435aa98b2b068b6eb64a6f15ec
mihue14 <samirjose675@gmail.com>
2022-10-12 12:59:41 -0300

    prueba git

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        prueba branch 2
+        prueba branch 3
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
