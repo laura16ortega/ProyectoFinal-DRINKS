a757651e102c43390731b17d5348e5bd42c4e7d5
santiago hurtado <sahurtaro@gmail.com>
2022-10-15 19:09:09 -0500

    SINGLE PRODUCT ROUTE

--- Server/Routes/ProductRoutes.js
+++ Server/Routes/ProductRoutes.js
@@ -4,11 +4,25 @@ import Product from "../src/models/productModel.js";
 
 const productRoute = express.Router();
 
+//GET ALL PRODUCTS
 productRoute.get(
   "/",
   asyncHandler(async (req, res) => {
     const products = await Product.find({});
     res.json(products);
+  })
+);
+
+//GET SINGLE PRODUCT
+productRoute.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      throw new Error("Product not found");
+    }
   })
 );
 
