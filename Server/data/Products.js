ec8da227d3519234c30e4f87a8216d4e96af8dce
santiago hurtado <sahurtaro@gmail.com>
2022-10-16 13:53:33 -0500

    Productos nuevos agregados al archivo y posteriormente a la base de datos

--- Server/data/Products.js
+++ Server/data/Products.js
@@ -1,8 +1,8 @@
 const productos = [
   {
-    id: "1",
     name: "Cerveza BBC",
-    image: "fdsfds",
+    image:
+      "https://www.bbccerveceria.com/sites/g/files/yrakuj1226/files/2022-06/CAJICAL_0.png",
     description: "Cerveza artesanal",
     price: 7000,
     stock: 50,
@@ -11,15 +11,114 @@ const productos = [
     category: "Cerveza",
   },
   {
-    id: "2",
-    name: "Vinito Sansón",
-    image: "fdsfds",
+    name: "Vino Sansón",
+    image:
+      "https://exitocol.vtexassets.com/arquivos/ids/2037679/VINO-MAESTRO-1570207_a.jpg?v=637268915226570000",
     description: "Vinito para cocinar",
     price: 25000,
     stock: 30,
     rating: 5,
     numReviews: 80,
     category: "Vino",
+  },
+  {
+    name: "Whiskhey Jack Daniels",
+    image:
+      "https://i0.wp.com/www.licoresmedellin.com/wp-content/uploads/2021/08/Jack_Daniels_N7_750ml-1.jpg?fit=600%2C800&ssl=1",
+    description: "Whiskey Escocés",
+    price: 80000,
+    stock: 36,
+    rating: 5,
+    numReviews: 26,
+    category: "Spirits",
+  },
+  {
+    name: "Champagne Laurent Perrier La Cuve",
+    image:
+      "https://i0.wp.com/www.licoresmedellin.com/wp-content/uploads/2021/08/Laurent-Perrier-La-Cuve-Brut-750ml.jpg?fit=600%2C800&ssl=1",
+    description: "Champagne",
+    price: 389000,
+    stock: 20,
+    rating: 4,
+    numReviews: 26,
+    category: "Vino",
+  },
+  {
+    name: "Champagne Mumm Cordon Rouge",
+    image:
+      "https://i0.wp.com/www.licoresmedellin.com/wp-content/uploads/2022/02/champagne-mumm-2.jpg?fit=600%2C800&ssl=1",
+    description: "Champagne",
+    price: 248000,
+    stock: 20,
+    rating: 4,
+    numReviews: 26,
+    category: "Vino",
+  },
+  {
+    name: "Vino Trivento Malbec",
+    image:
+      "https://i0.wp.com/www.licoresmedellin.com/wp-content/uploads/2021/08/trivento_Malbec_750ml-licores_medellin.jpg?fit=600%2C800&ssl=1",
+    description: "Vino reserva",
+    price: 248000,
+    stock: 20,
+    rating: 4,
+    numReviews: 26,
+    category: "Vino",
+  },
+  {
+    name: "Vino Espumoso Chandon",
+    image:
+      "https://i0.wp.com/www.licoresmedellin.com/wp-content/uploads/2021/08/chandon-demisec-nuevo.jpg?fit=600%2C800&ssl=1",
+    description: "Vino espumoso",
+    price: 608000,
+    stock: 56,
+    rating: 4,
+    numReviews: 69,
+    category: "Vino",
+  },
+  {
+    name: "Cerveza 3 Cordilleras Mona 330ml Four Pack",
+    image:
+      "https://i0.wp.com/www.licoresmedellin.com/wp-content/uploads/2021/08/4-Pack-3-cordilleras-MONA-SV-1.jpg?fit=600%2C800&ssl=1",
+    description: "Cerveza artesanal",
+    price: 21000,
+    stock: 58,
+    rating: 4,
+    numReviews: 47,
+    category: "Cerveza",
+  },
+  {
+    name: "Cerveza Delirium Tremens",
+    image:
+      "https://i0.wp.com/www.licoresmedellin.com/wp-content/uploads/2021/08/delirium-tremens.jpg?fit=600%2C800&ssl=1",
+    description: "Cerveza importada Belga",
+    price: 19900,
+    stock: 25,
+    rating: 5,
+    numReviews: 74,
+    category: "Cerveza",
+  },
+  {
+    name: "Cerveza Floris Passion",
+    image:
+      "https://i0.wp.com/www.licoresmedellin.com/wp-content/uploads/2021/08/floris-passion-nueva.jpg?fit=600%2C800&ssl=1",
+    description: "Cerveza importada Belga",
+    price: 21500,
+    stock: 96,
+    rating: 5,
+    numReviews: 47,
+    category: "Cerveza",
+  },
+  {
+    name: "Vodka Smirnoff Lulo",
+    image:
+      "https://i0.wp.com/www.licoresmedellin.com/wp-content/uploads/2021/08/smirnoff-lulo-375-2.jpg?fit=600%2C800&ssl=1",
+    description: "Vodka media 375ml",
+    price: 23000,
+    stock: 96,
+    rating: 4,
+    numReviews: 74,
+    category: "Spirits",
   },
 ];
 
