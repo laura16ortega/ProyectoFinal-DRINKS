3a36089f13fd7b2b4c14b6c4b405a9f0c4255113
kachamozo <kachamozo@yahoo.com>
2022-10-18 18:52:03 -0400

    login api

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,20 +1,29 @@
-import express from "express";
-import productos from "../data/Products.js";
-import products from "./routes/product.route.js";
-import dotenv from "dotenv";
-import connectDatabase from "../config/MongoDb.js";
-import importData from "../DataImport.js";
-import productRoute from "../Routes/ProductRoutes.js";
-import { errorHandler, notFound } from "../Middleware/Errors.js";
+import express from 'express';
+import cors from 'cors';
+import productos from '../data/Products.js';
+import products from './routes/product.route.js';
+import dotenv from 'dotenv';
+import connectDatabase from '../config/MongoDb.js';
+import importData from '../DataImport.js';
+import productRoute from '../Routes/ProductRoutes.js';
+import { errorHandler, notFound } from '../Middleware/Errors.js';
+import userRouter from '../Routes/UserRoutes.js';
 
 dotenv.config();
 connectDatabase();
+
 const app = express();
+//ARREGLA PROBLEMA DE CORS
+app.use(cors());
+app.use(express.json());
+
 const PORT = process.env.PORT || 3001;
 
 //API
-app.use("/api/import", importData);
-app.use("/api/products", productRoute);
+
+app.use('/api/import', importData);
+app.use('/api/products', productRoute);
+app.use('/api/users', userRouter);
 //ERROR HANDLER
 app.use(notFound);
 app.use(errorHandler);
@@ -24,15 +33,15 @@ app.use(errorHandler);
 // });
 
 // LOAD SINGLE PRODUCT FROM SERVER
-app.get("/api/products/:id", (req, res) => {
-  const product = productos.find((p) => p.id === req.params.id);
-  res.json(product);
+app.get('/api/products/:id', (req, res) => {
+	const product = productos.find((p) => p.id === req.params.id);
+	res.json(product);
 });
 
 // app.get("/", (req, res) => {
 //   res.send("API is running...");
 // });
 
-app.listen(PORT, () => console.log("Server iniciado en el puerto: " + PORT));
+app.listen(PORT, () => console.log('Server iniciado en el puerto: ' + PORT));
 
-app.use("/products", products);
+app.use('/products', products);
--- Server/src/models/userModel.js
+++ Server/src/models/userModel.js
@@ -1,40 +1,45 @@
 // import { Schema, model } from "mongoose";
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema(
-  {
-    fullName: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    isAdmin: {
-      type: Boolean,
-      required: true,
-      default: false,
-    },
-    phone_number: {
-      type: Number,
-      required: true,
-    },
-    address: {
-      type: String,
-    },
-  },
-  {
-    timestamps: true,
-    versionKey: false,
-  }
+	{
+		fullName: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		isAdmin: {
+			type: Boolean,
+			required: true,
+			default: false,
+		},
+		phone_number: {
+			type: Number,
+			required: true,
+		},
+		address: {
+			type: String,
+		},
+	},
+	{
+		timestamps: true,
+		versionKey: false,
+	}
 );
 
-const User = mongoose.model("User", userSchema);
+//LOGIN
+userSchema.methods.matchPassword = async function (enterPassword) {
+	return await bcrypt.compare(enterPassword, this.password);
+};
+const User = mongoose.model('User', userSchema);
 
 export default User;
