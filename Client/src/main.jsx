5a06262024bde4ce75f6dd13c9fa662f0ee99755
aribxax <aribxax@gmail.com>
2022-10-19 22:58:54 +0200

    small fixes and traducido a espaniol

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -5,13 +5,26 @@ import './index.css'
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './redux/store/store.js';
+import { Auth0Provider } from '@auth0/auth0-react'
+
+
+
+
+const DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN;
+const CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Auth0Provider domain={DOMAIN} clientId={CLIENT_ID}
+    redirectUri={window.location.origin}>    
+      <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>  
+    </Auth0Provider>
+
   </React.StrictMode>
 );
