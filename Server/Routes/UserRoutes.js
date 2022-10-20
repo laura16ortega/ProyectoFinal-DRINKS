68681ad359cfa9e0f25a4394684b6e1da2a42ec8
kachamozo <kachamozo@yahoo.com>
2022-10-19 22:39:43 -0400

    register-api carpeta  rutas y controller eliminado

--- Server/Routes/UserRoutes.js
+++ Server/Routes/UserRoutes.js
@@ -1,30 +1,61 @@
-import express from "express";
-import asyncHandler from "express-async-handler";
-import User from "../src/models/userModel.js";
-import generateToken from "../utils/generateToken.js";
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+import User from '../src/models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 const userRouter = express.Router();
 
 //LOGIN
 userRouter.post(
-  "/login",
-  asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (user && (await user.matchPassword(password))) {
-      res.json({
-        _id: user._id,
-        fullName: user.fullName,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token: generateToken(user._id),
-        createdAt: user.createdAt,
-      });
-    } else {
-      res.status(401);
-      throw new Error("Invalid Email or Password");
-    }
-  })
+	'/login',
+	asyncHandler(async (req, res) => {
+		const { email, password } = req.body;
+		const user = await User.findOne({ email });
+		if (user && (await user.matchPassword(password))) {
+			res.json({
+				_id: user._id,
+				fullName: user.fullName,
+				email: user.email,
+				isAdmin: user.isAdmin,
+				token: generateToken(user._id),
+				createdAt: user.createdAt,
+			});
+		} else {
+			res.status(401);
+			throw new Error('Invalid Email or Password');
+		}
+	})
+);
+
+//REGISTER
+userRouter.post(
+	'/',
+	asyncHandler(async (req, res) => {
+		const { fullName, email, password, phone_number } = req.body;
+		const userExists = await User.findOne({ email });
+
+		if (userExists) {
+			res.status(400);
+			throw new Error('User alrady exists');
+		}
+
+		const user = await User.create({
+			fullName,
+			email,
+			password,
+			phone_number,
+		});
+
+		if (user) {
+			res.status(201).json({
+				_id: user._id,
+				fullName: user.fullName,
+				email: user.email,
+				isAdmin: user.isAdmin,
+				token: generateToken(user._id),
+			});
+		}
+	})
 );
 
 export default userRouter;
--- Server/src/controllers/product.controller.js
+++
@@ -1,6 +0,0 @@
-import Product from "../models/productModel.js";
-
-export const getProducts = async (req, res) => {
-  let products = await Product.find();
-  res.status(200).send(products);
-};
