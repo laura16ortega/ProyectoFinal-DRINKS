581f5e6fd49fc00a387651a70d8e8edebcf53f2a
aribxax <aribxax@gmail.com>
2022-10-23 15:00:00 +0200

    update

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -14,7 +14,7 @@ import Cart from './components/Cart/Cart';
 import FavoriteProducts from './components/FavoriteProducts/FavoriteProducts';
 import Contact from './components/Contact/Contact';
 
-import LoginButton from './components/LogginButton/LoginButton';
+import LoginButton from './components/LoginButton/LoginButton';
 import LogoutButton from './components/LogoutButton/LogoutButton';
 import Profile from './pages/Profile/Profile';
 import { useAuth0 } from '@auth0/auth0-react';
--- Client/src/redux/actions/index.js
+++ Client/src/redux/actions/index.js
@@ -111,6 +111,7 @@ export const getAllCategories = () => {
 //}
 
 
+
 export const productSearch = (name) => {
     return async (dispatch) => {
         try {
